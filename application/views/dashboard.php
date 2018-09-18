1825a971c79a9eac2253cf3324147e32abe85048
volatiana <volatiana.helisoa@yahoo.fr>
2018-09-18 11:45:35 +0300

    dashbord-domaine

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,13 +1,60 @@
 
-
-<a href="<?php echo site_url('user/index'); ?>" >Utilisateur </a>
-
-<a href="<?php echo site_url('domaine/index'); ?>" >Domaine </a>
-
-<a href="<?php echo site_url('registrar/index'); ?>" >Registrar </a>
-
-<a href="<?php echo site_url('hebergement/index'); ?>" >Hebergement </a>
-
-<a href="<?php echo site_url('ip/index'); ?>" >IP </a>
-
-
+    <div class="cont-dash clearfix">
+        <div class="left-dash">
+            <div class="dash-item1">
+                <div class="content">
+                    <div>
+                    <a href="<?php echo site_url('domaine/index'); ?>" >
+                        Nombre de <span>NDD</span>
+                        <div class="display-nbr">1000</div>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <div class="dash-item2">
+                <div class="content">
+                    <div>
+                    <a href="<?php echo site_url('registrar/index'); ?>" > 
+                        <span>Registrar</span>
+                        <div class="display-nbr">125</div>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <div class="dash-item3">
+                <div class="content">
+                    <div>
+                    <a href="<?php echo site_url('hebergement/index'); ?>" >
+                        Nombre<span>d’Hebergement</span>
+                        <div class="display-nbr">685</div>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <div class="dash-item4">
+                <div class="content">
+                    <div>
+                    <a href="<?php echo site_url('ip/index'); ?>" >
+                        Nombre <span>d'ip</span>
+                        <div class="display-nbr">2535</div>
+                    </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="right-dash">
+            <div class="bloc-graph">
+                <!-- content graph -->
+            </div>
+            <div class="dash-item5">
+                <div class="content">
+                    <div>
+                    <a href="<?php echo site_url('user/index'); ?>" >
+                        <span>Utilisateurs</span>
+                        <div class="display-nbr">182</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
