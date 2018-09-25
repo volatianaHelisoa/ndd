09bb9ef9fdf395af01121fc737421ad844fdde0e
Unknown <zo.ravalitera@outlook.com>
2018-09-25 10:19:40 +0300

    Validation mot de passe

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -39,20 +39,52 @@
 		<input type="text" name="email" value="<?php echo ($this->input->post('email') ? $this->input->post('email') : $t_user['email']); ?>">
 	</div>
 	
-	<div class="step-after" style=" clear: both;">
+	<div class="step-after clearfix" style=" clear: both;">
 		<div class="ttl-infos clearfix">
 			<span>Changer mot de passe</span>
 		</div>
 		<div class="field half">
 			<label for="">Nouveau mot de passe : </label>
-			<input type="password" name="password"  value="<?php echo ($this->input->post('password') ? $this->input->post('password') : $t_user['password']); ?>">
+			<input type="password" name="password" id="newpass1" class="pass-field">
 		</div>
 		<div class="field half">
 			<label for="">Répétér nouveau mot de passe :</label>
-			<input type="text">
+			<input type="text" id="newpass2" class="pass-field">
 		</div>
+		
 	</div>
+	<div id="mdp-equality" class="clearfix"></div>
+		<ul class="policies">
+			<li id="pass-length">Au moin caractère 8</li>
+			<li id="pass-min-maj">Utilisez des MAJ et des MIN</li>
+			<li id="pass-special">Utilisez des caractères spéciaux</li>
+			<li id="pass-number">Pensez à ajouter des chiffres</li>
+		</ul>  
+	<div id="result"></div> 
 	<button type="submit" class="submit">Enregistrer</button>
 </div>
 	
 <?php echo form_close(); ?>
+
+<script type="text/javascript">
+	function checkStrength(s) {
+		var a = 0;
+		return s.length >= 8 ? (a += 1, $("#pass-length").removeClass(), $("#pass-length").addClass("valid")) : ($("#result").addClass("short"), $("#pass-length").removeClass(), $("#pass-length").addClass("invalid")),
+		s.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/) && s.length > 0 ? (a += 1, $("#pass-min-maj").removeClass(), $("#pass-min-maj").addClass("valid")) : ($("#pass-min-maj").removeClass(), $("#pass-min-maj").addClass("invalid")),
+		s.match(/([0-9])/) && null != s.length ? (a += 1, $("#pass-number").removeClass(), $("#pass-number").addClass("valid")) : ($("#pass-number").removeClass(), $("#pass-number").addClass("invalid")),
+		s.match(/([!,%,&,@,#,$,^,*,?,_,~])/) && null != s.length ? (a += 1, $("#pass-special").removeClass(), $("#pass-special").addClass("valid")) : ($("#pass-special").removeClass(), $("#pass-special").addClass("invalid")),
+		s.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/) && null != s.length && (a += 1),
+		a < 2 ? ($("#result").removeClass(), $("#result").addClass("faible"), "Mot de passe trop Faible") : 2 == a ? ($("#result").removeClass(), $("#result").addClass("bon"), "Mot de passe moyen") : ($("#result").removeClass(), $("#result").addClass("fort"), "Mot de passe fort")
+	}
+
+	function checkEquality() {
+		var s = $("#newpass1").val(),
+			a = $("#newpass2").val();
+		a && s == a ? (console.log("pass"), $("#mdp-equality").html('Vos mots de passe concordent')) : $("#mdp-equality").html('Les deux mots de passes ne sont pas identiques')
+	}
+	$("#newpass1").keyup(function() {
+		$("#result").html(checkStrength($("#newpass1").val()))
+	}), $(".pass-field").keyup(function() {
+		checkEquality()
+	});
+</script>
