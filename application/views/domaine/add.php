43d1f39c6e6058fcac481ec5c8dfc60782aeaede
Unknown <zo.ravalitera@outlook.com>
2018-10-11 15:17:12 +0300

    Dynamic domain's number

--- application/views/domaine/add.php
+++ application/views/domaine/add.php
@@ -1,119 +1,117 @@
 <?php echo form_open('domaine/add',array("class"=>"form-horizontal")); ?>
 
-                <div class="head-section centered-el">
-                    <span class="title-l">Nom de domaine</span>
-                    <p>Vous avez 1000 nom de domaines</p>
-                </div>				
-				
-                <div class="wrap-field info-gen carte">
-						<div class="title-field">Information générale</div>
-						<?php if(isset($error_nom)) :?>
-							<div class="alert alert-info" role="alert">
-								<?php echo $error_nom; ?>
-							</div>
-						<?php endif ?>
-                    	<div class="field">
-                        	<label for="">Nom de domaine (*) :</label>
-							<input type="text" name="nom" value="<?php echo $this->input->post('nom'); ?>"  id="nom" >
-						</div>
-                        <div class="field">
-                        	<label for="">Thématique :</label>
-                        	<input type="text" id="theme" name="theme" placeholder="Saisir thème" >
-                        </div>
-                        
-                        <div class="field">
-                        	<label for="">Hébegement :</label>
-                        	<select name="id_heberg"  id="id_heberg">
-							<option value="">Selectionner hebergement</option>
-							<?php 
-							foreach($all_t_hebergement as $t_hebergement)
-							{
-								$selected = ($t_hebergement['id'] == $this->input->post('id_heberg')) ? ' selected="selected"' : "";
-
-								echo '<option value="'.$t_hebergement['id'].'" '.$selected.'>'.$t_hebergement['url'].'</option>';
-							} 
-							?>
-						</select>
-                        </div>
-						<div class="field div-addr-ip" >
-                        	<label for="">Adresse IP :</label>
-                        	<select id="addr-ip"   name="addr-ip" ?>" >
-							</select>
-                        </div>
-                        <div class="field">
-                        	<label for="">Registrar (*) :</label>
-                        	<select name="id_registrar"  >
-								<option value="">Selectionner registrar</option>
-								<?php 
-								foreach($all_t_registrar as $t_registrar)
-								{
-									$selected = ($t_registrar['id'] == $this->input->post('id_registrar')) ? ' selected="selected"' : "";
-
-									echo '<option value="'.$t_registrar['id'].'" '.$selected.'>'.$t_registrar['name'].'</option>';
-								} 
-								?>
-							</select>
-                        </div>
-                        <input type="button" class="btn submit btn-next prevnext" value="Suivant">
-						<input type="submit" class="btn submit btn-save-first" value="Ajouter">
-                </div>
-                <div class="wrap-field preference carte">
-					<div class="title-field ">Préférences</div>
-                   
-                    	<div class="field">
-                        	<label for="">CMS / HTML</label>
-                        	<select name="id_cms" id="cms_select">
-								<option value="">Selectionner cms</option>
-								<?php 
-								foreach($all_t_cms as $t_cms)
-								{
-									$selected = ($t_cms['id'] == $this->input->post('id_cms')) ? ' selected="selected"' : "";
-
-									echo '<option value="'.$t_cms['id'].'" '.$selected.'>'.$t_cms['type'].'</option>';
-								} 
-								?>
-							</select>
-                        </div>
-                        <div class="sub-title">Accès FTP</div>
-                        <div class="field">
-                        	<label for="">Serveur</label>
-                        	<input type="text" name="ftp_server" value="<?php echo $this->input->post('ftp_server'); ?>"  id="ftp_server" />
-                        </div>
-                        <div class="field">
-                        	<label for="">Login :</label>
-							<input type="text" name="ftp_login" value="<?php echo $this->input->post('ftp_login'); ?>"  id="ftp_login" />
-                        </div>
-                        <div class="field">
-                        	<label for="">Mot de passe :</label>
-                        	<input type="text" name="ftp_password" value="<?php echo $this->input->post('ftp_password'); ?>" id="ftp_password" />
-						</div>
-						<div id="bo-acces">
-							<div class="sub-title">Administration</div>
-							<div class="field">
-								<label for="">URL :</label>
-								<input type="text" name="admin_url" value="<?php echo $this->input->post('admin_url'); ?>" id="admin_url" />
-							</div>
-							<div class="field">
-								<label for="">Login :</label>
-								<input type="text" name="admin_login" value="<?php echo $this->input->post('admin_login'); ?>" id="admin_login" />
-							</div>
-							<div class="field">
-								<label for="">Mot de passe :</label>
-								<input type="text" name="admin_password" value="<?php echo $this->input->post('admin_password'); ?>"  id="admin_password" />
-							</div>
-						
-							<div class="sub-title">Plugin</div>
-							<div class="content-chips">
-								<input class="typeahead" name="techno_tags" type="text" data-role="materialtags" >						
-							</div>
-						</div>
-						<input type="button" class="btn submit btn-previous prevnext" value="Precedent">
-						<input type="submit" class="btn submit btn-save" value="Ajouter">
-                </div>
-						
+	<div class="head-section centered-el">
+		<span class="title-l">Nom de domaine</span>
+		<p>Vous avez <?php $domaine_data = $this->Domaine_model->get_all_t_domaine(); echo(count($domaine_data));?> nom de domaines</p>
+		
+		
+	</div>				
+	
+	<div class="wrap-field info-gen carte">
+			<div class="title-field">Information générale</div>
+			<?php if(isset($error_nom)) :?>
+				<div class="alert alert-info" role="alert">
+					<?php echo $error_nom; ?>
+				</div>
+			<?php endif ?>
+			<div class="field">
+				<label for="">Nom de domaine (*) :</label>
+				<input type="text" name="nom" value="<?php echo $this->input->post('nom'); ?>"  id="nom" >
+			</div>
+			<div class="field">
+				<label for="">Thématique :</label>
+				<input type="text" id="theme" name="theme" placeholder="Saisir thème" >
+			</div>
 			
-  
+			<div class="field">
+				<label for="">Hébegement :</label>
+				<select name="id_heberg"  id="id_heberg">
+				<option value="">Selectionner hebergement</option>
+				<?php 
+				foreach($all_t_hebergement as $t_hebergement)
+				{
+					$selected = ($t_hebergement['id'] == $this->input->post('id_heberg')) ? ' selected="selected"' : "";
 
+					echo '<option value="'.$t_hebergement['id'].'" '.$selected.'>'.$t_hebergement['url'].'</option>';
+				} 
+				?>
+			</select>
+			</div>
+			<div class="field div-addr-ip" >
+				<label for="">Adresse IP :</label>
+				<select id="addr-ip"   name="addr-ip" ?>" >
+				</select>
+			</div>
+			<div class="field">
+				<label for="">Registrar (*) :</label>
+				<select name="id_registrar"  >
+					<option value="">Selectionner registrar</option>
+					<?php 
+					foreach($all_t_registrar as $t_registrar)
+					{
+						$selected = ($t_registrar['id'] == $this->input->post('id_registrar')) ? ' selected="selected"' : "";
+
+						echo '<option value="'.$t_registrar['id'].'" '.$selected.'>'.$t_registrar['name'].'</option>';
+					} 
+					?>
+				</select>
+			</div>
+			<input type="button" class="btn submit btn-next prevnext" value="Suivant">
+			<input type="submit" class="btn submit btn-save-first" value="Ajouter">
+	</div>
+	<div class="wrap-field preference carte">
+		<div class="title-field ">Préférences</div>
+		
+			<div class="field">
+				<label for="">CMS / HTML</label>
+				<select name="id_cms" id="cms_select">
+					<option value="">Selectionner cms</option>
+					<?php 
+					foreach($all_t_cms as $t_cms)
+					{
+						$selected = ($t_cms['id'] == $this->input->post('id_cms')) ? ' selected="selected"' : "";
+
+						echo '<option value="'.$t_cms['id'].'" '.$selected.'>'.$t_cms['type'].'</option>';
+					} 
+					?>
+				</select>
+			</div>
+			<div class="sub-title">Accès FTP</div>
+			<div class="field">
+				<label for="">Serveur</label>
+				<input type="text" name="ftp_server" value="<?php echo $this->input->post('ftp_server'); ?>"  id="ftp_server" />
+			</div>
+			<div class="field">
+				<label for="">Login :</label>
+				<input type="text" name="ftp_login" value="<?php echo $this->input->post('ftp_login'); ?>"  id="ftp_login" />
+			</div>
+			<div class="field">
+				<label for="">Mot de passe :</label>
+				<input type="text" name="ftp_password" value="<?php echo $this->input->post('ftp_password'); ?>" id="ftp_password" />
+			</div>
+			<div id="bo-acces">
+				<div class="sub-title">Administration</div>
+				<div class="field">
+					<label for="">URL :</label>
+					<input type="text" name="admin_url" value="<?php echo $this->input->post('admin_url'); ?>" id="admin_url" />
+				</div>
+				<div class="field">
+					<label for="">Login :</label>
+					<input type="text" name="admin_login" value="<?php echo $this->input->post('admin_login'); ?>" id="admin_login" />
+				</div>
+				<div class="field">
+					<label for="">Mot de passe :</label>
+					<input type="text" name="admin_password" value="<?php echo $this->input->post('admin_password'); ?>"  id="admin_password" />
+				</div>
+			
+				<div class="sub-title">Plugin</div>
+				<div class="content-chips">
+					<input class="typeahead" name="techno_tags" type="text" data-role="materialtags" >						
+				</div>
+			</div>
+			<input type="button" class="btn submit btn-previous prevnext" value="Precedent">
+			<input type="submit" class="btn submit btn-save" value="Ajouter">
+	</div>
 <?php echo form_close(); ?>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
  
